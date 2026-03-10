7883874b0676776fa3ed7aec4d56a60e33e20026
vklata <151921976+vklata@users.noreply.github.com>
2026-03-10 20:09:47 +0530

    Fix import case for Auth page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Auth from './pages/auth'
+import Auth from './pages/Auth'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
